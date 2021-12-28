2e5963870e9d3506815fafb898f3ba0021d9c123
Ogura <okqsmail@gmail.com>
2021-12-28 19:11:57 +0900

    パス再度更新

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Build.loader.json");  
+const unityInstance = UnityLoader.instantiate("unityContainer", "/../Build/Build.loader.json");  
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
