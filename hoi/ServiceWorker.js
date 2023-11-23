2249a8ec9532a9ec22b6f5fb0a32eb521a1f1ccc
Ogura <okqsmail@gmail.com>
2023-11-24 05:05:40 +0900

    あっちむいてホイを追加

---
+++ hoi/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "OguLabo-VRM_AcchiMuite_Hoi-2023.11.27";
+const contentToCache = [
+    "Build/hoi.loader.js",
+    "Build/hoi.framework.js.br",
+    "Build/hoi.data.br",
+    "Build/hoi.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
